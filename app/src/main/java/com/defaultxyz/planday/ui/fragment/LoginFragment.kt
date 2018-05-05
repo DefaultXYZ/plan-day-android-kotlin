50d93d8812a22d7952b385c6214209e43e8c6cdf
Yevhenii Ishchenko <yevhenii.i@hexagen48.com>
2018-05-05 17:20:00 +0200

    Implement sign in, sign up fragment layouts

--- app/src/main/java/com/defaultxyz/planday/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/defaultxyz/planday/ui/fragment/LoginFragment.kt
@@ -8,12 +8,33 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.defaultxyz.planday.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_sign_up.setOnClickListener { onSignUp() }
+        btn_sign_in.setOnClickListener { onSignIn() }
+    }
+
+    private fun onSignUp() {
+        val fragment = SignUpFragment()
+        activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.container, fragment, SignUpFragment.TAG)
+                ?.addToBackStack(null)?.commit()
+    }
+
+    private fun onSignIn() {
+        val fragment = SignInFragment()
+        activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.container, fragment, SignInFragment.TAG)
+                ?.addToBackStack(null)?.commit()
     }
 
     companion object {
---
+++ app/src/main/java/com/defaultxyz/planday/ui/fragment/SignUpFragment.kt
@@ -0,0 +1,20 @@
+package com.defaultxyz.planday.ui.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.defaultxyz.planday.R
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    companion object {
+        const val TAG = "SignUpFragment"
+    }
+}
